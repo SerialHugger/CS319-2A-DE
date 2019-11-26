3d87ed7227e247f564500dd59a0c842d0aae8b79
MBerk <m.berkose@gmail.com>
2019-11-26 22:18:37 +0300

    added fps counter

--- project_folder/src/main/java/org/openjfx/MainGame.java
+++ project_folder/src/main/java/org/openjfx/MainGame.java
@@ -24,7 +24,9 @@ public class MainGame {
     public void update(Game game){
         gameController.update();
     }
-
+    public void updateInteraction(Game game){
+        gameController.updateInteraction();
+    }
     public void setButtonHandler(Scene scene) { gameController.setButtonHandler(scene);}
 
 }
