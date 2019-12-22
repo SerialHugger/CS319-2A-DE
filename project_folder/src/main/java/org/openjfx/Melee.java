1cf6c65873d94b719b9d31a92a3f90d9527673c4
MBerk <m.berkose@gmail.com>
2019-12-22 18:30:47 +0300

    .Berk: Auto stash before merge of "M.Berk" and "origin/tolga"

--- project_folder/src/main/java/org/openjfx/Melee.java
+++ project_folder/src/main/java/org/openjfx/Melee.java
@@ -34,7 +34,7 @@ public class Melee extends PlayerEquipment {
         else
             setX(player.body.getTranslateX());
 
-        setY(player.body.getTranslateY() - player.height * 1.5);
+        setY(player.body.getTranslateY() - player.height / 2);
 
         // rotate the arm
         currentAngle += ROTATION_INCREMENT;
