c19035faf6126d3a9c5c59a76717d21a66c5ec80
yasinkokturk <39164550+yasinkokturk@users.noreply.github.com>
2019-12-23 16:44:38 +0300

    Civilian updated

--- project_folder/src/main/java/org/openjfx/GameComponent/Civilian.java
+++ project_folder/src/main/java/org/openjfx/GameComponent/Civilian.java
@@ -1,20 +1,71 @@
 package org.openjfx.GameComponent;
 
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.ImagePattern;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
 
 public class Civilian extends GameComponent {
     boolean grabbed;
-
-    Civilian(double givenWidth, double givenHeight, ImagePattern asset) {
-        super(givenWidth, givenHeight, "civilian");
-        this.height = magicConverter(80);
-        this.width = magicConverter(60);
+    boolean targeted;
+    boolean saved;
+    Civilian (double width, double height, ImagePattern asset){
+        super(width, height, "civilian");
+        grabbed = false;
+        targeted = false;
+        saved = false;
+        double startingX = (Math.random() * (1920  * 4)) + (1920 * -2) ;
+        double startingY = 960;
+        body = new Rectangle(width, height, null);
         body.setFill(asset);
+        body.setTranslateX(startingX);
+        body.setTranslateY(startingY);
+        speed_x = 0;
+        speed_y = 0;
+        hitBoxes = new Shape[1];
+        hitBoxes[0] = new ComponentHitBoxRectangle(width, height, "civilian", type);
+        hitBoxes[0].setTranslateX(startingX);
+        hitBoxes[0].setTranslateY(startingY);
+        maxSpeed = 6;
+        acceleration = magicConverter(0.4);
     }
 
-    public void moveCivilian(GameComponentFactory GCF, Player player, boolean left) {
-        if (grabbed) {
-
+    public void moveCivilian(GameComponentFactory GCF, Pane gameRoot, Player player, boolean left , int speedFactor){
+        if (grabbed){
+                speed_x = 0;
+                speed_y = 5;
+                moveY(-1, speed_y);
         }
+        else if (this.getY() < 960){
+            speed_y = speed_y + acceleration;
+            moveY(1, speed_y);
+            System.out.println(speed_y);
+        }
+        else if (this.getY() >= 960){
+            if (speed_y > maxSpeed)
+                dead = true;
+        }
+        loopAroundTheMap(GCF.width, player, left);
+    }
+    public void loopAroundTheMap(double bgWidth, Player player, boolean left) {
+        //updates the space ships so they loop around map
+        if (left) { // if button to go left, "A", is pressed
+            if (body.getTranslateX() > player.getX() + (2 * bgWidth)) { // if he enemy ship is width * 2 away from the ship teleport it to other side
+                for (Shape hitBox : hitBoxes) {
+                    hitBox.setTranslateX(hitBox.getTranslateX() - (4 * bgWidth));
+                }
+                body.setTranslateX(body.getTranslateX() - (4 * bgWidth));
+            }
+        } else { // if button to go right, "D", is pressed
+            if (body.getTranslateX() < player.getX() - (2 * bgWidth)) { // if he enemy ship is width * 2 away from the ship teleport it to other side
+                for (Shape hitBox : hitBoxes) {
+                    hitBox.setTranslateX(hitBox.getTranslateX() + (4 * bgWidth));
+                }
+                body.setTranslateX(body.getTranslateX() + (4 * bgWidth));
+            }
+        }
+    }
+    public void setGrabbed(boolean status){
+        grabbed = status;
     }
 }
